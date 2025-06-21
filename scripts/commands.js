0f2a435cfc6f9368f663ebbd0ae87b0d3315324c
justDemian <demian.bale.aldea@gmail.com>
2025-06-20 21:07:31 -0400

    quick_fix

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,36 +1,45 @@
 const command_input = document.getElementById("direct-input");
 const command_history = document.getElementById("input-lines");
-let not_unknow = true;
+let knowed = true;
 
 command_input.addEventListener('keydown', function(event){
   if (event.key === "Enter"){
     // Crear un nuevo <p> en history
-    const text = command_input.value.trim();
-    text.toLowerCase();
+    const cmd = command_input.value.trim().toLowerCase().split(/\s+/);
+
+    // rutt
+    if (cmd[0] === 'rutt'){
+      knowed = true;
+      rutt(cmd);
+    }
 
     // Limpiar el historial
-    if (text === 'clear'){
-      not_unknow = true;
+    else if (cmd[0] === 'clear' || cmd[0] === 'cls'){
+      knowed = true;
       command_history.innerHTML = '';
     }
 
-    // rutt
-    if (text.startsWith('rutt')){
-      not_unknow = true;
-      add_line('<span class="cyn">RUT</span> iniciado');
+    // Se escribe algo pero no se reconoce
+    else if (cmd[0] !== '' && knowed){
+      knowed = false;
+      line('<span class="red">Comando desconocido</span> - intenta otra vez')
     }
 
-    // Se escribe algo pero no se reconoce
-    else if (text !== '' && not_unknow){
-      not_unknow = false;
-      add_line('<span class="red">Comando desconocido</span> - intenta otra vez');
-    }
     command_input.value = "";
   }
 })
 
-const add_line = (content)=>{
+const rutt = (cont)=>{
+  let info = '';
+  line('ola')
+  for (let i = 0; i < cont.lenght; i++) {
+    info = cont[i] + 'hola'
+    line('a');
+  }
+}
+
+const line = (cont)=>{
   const new_entry = document.createElement('p');
-  new_entry.innerHTML = content;
+  new_entry.innerHTML = cont;
   command_history.appendChild(new_entry);
 }

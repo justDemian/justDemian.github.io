9705e965ef3430b2ab357d8e1c8113edd94485f7
justDemian <demian.bale.aldea@gmail.com>
2025-07-07 17:50:52 -0400

    start_some_play

--- not21/21_thingy.js
+++ not21/21_thingy.js
@@ -7,3 +7,40 @@ function esDispositivoMovil() {
 if (esDispositivoMovil()) {
   document.getElementById("view_mode").href = 'mobile.css';
 }
+
+
+// Creación de la baraja
+let cards = [];
+let symbols = ["♠", "♥", "♦", "♣"];
+let new_card = "";
+let category = "";
+
+for (let symbol=0 ; symbol<=3 ; symbol++) {
+  for (let i=1 ; i<=13 ; i++){
+    switch(i){
+      case 1:
+        category = "A";
+        break;
+
+      case 11:
+        category = "J";
+        break;
+
+      case 12:
+        category = "Q";
+        break;
+
+      case 13:
+        category = "K";
+        break;
+      
+      default:
+        category = i.toString();
+    }
+    new_card = category + symbols[symbol];
+    cards.push(new_card);
+  }
+}
+
+// Iniciar Juego
+let house = document.getElementById();
